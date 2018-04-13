e9c6432c2bd94049109c7b1398566d4dd7f2a58b
Casper <aaa40066@gmail.com>
2018-04-13 20:56:43 +0800

    HW2

--- News_Final_source_code.py
+++ News_Final_source_code.py
@@ -135,3 +135,153 @@ for i in range(4):
 	topic_sentimentheadline = topic_sentimentheadline + [(topics[i] , topic_sentimentheadline[i][1] / total_topic[i][1])]
 
 #4
+news = sqlContext.read.format('com.databricks.spark.csv').options(header='true').load('hdfs://master:9000/user/HW2/News_Final.csv')
+title_forall = news.select("Title","Topic").rdd.map(list)
+headline_forall = news.select("Headline","Topic").rdd.map(list)
+
+title_pertopic = title_forall.map(lambda x:[x[0],x[1]])
+title_pertopic = title_pertopic.map(lambda x:(x[0].split(' ') ,x[1]))
+title_pertopic = title_pertopic.flatMap(lambda x:[(element, x[1]) for element in x[0]])
+title_pertopic = title_pertopic.map(lambda x:(x,1))
+from operator import add
+title_pertopic = title_pertopic.reduceByKey(add)
+title_pertopic = title_pertopic.sortBy(lambda x: x[1], False)
+title_pertopic = title_pertopic.map(lambda x: x[0])
+
+headline_pertopic = headline_forall.map(lambda x:[x[0],x[1]])
+headline_pertopic = headline_pertopic.filter(lambda x:type(x[0]) == str)
+headline_pertopic = headline_pertopic.map(lambda x:(x[0].split(' '),x[1]))
+headline_pertopic = headline_pertopic.flatMap(lambda x:[(element, x[1])for element in x[0]])
+headline_pertopic = headline_pertopic.map(lambda x:(x,1))
+headline_pertopic = headline_pertopic.reduceByKey(add)
+headline_pertopic = headline_pertopic.sortBy(lambda x:x[1],False)
+headline_pertopic = headline_pertopic.map(lambda x: x[0])
+
+#topic_obama_fortitle
+
+topic_obama_fortitle = title_pertopic.filter(lambda x:x[1] == 'obama').map(lambda x:x[0]).take(100)
+topic_obama = title_forall.filter(lambda x:x[1] == 'obama').map(lambda x:x[0]).collect()
+obama_co_matrix_fortitle = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_obama)):
+			if topic_obama_fortitle[i] in topic_obama[k] and topic_obama_fortitle[j] in topic_obama[k]:
+				obama_co_matrix_fortitle[i][j] = obama_co_matrix_fortitle[i][j] + 1
+
+obama_co_matrix_fortitle_output = open('obama_co_matrix_fortitle_output.txt','w')
+obama_co_matrix_fortitle_output.write(str(obama_co_matrix_fortitle))
+obama_co_matrix_fortitle_output.close()
+
+#topic_economy_fortitle
+
+topic_economy_fortitle = title_pertopic.filter(lambda x:x[1] == 'economy').map(lambda x:x[0]).take(100)
+topic_economy = title_forall.filter(lambda x:x[1] == 'economy').map(lambda x:x[0]).collect()
+economy_co_matrix_fortitle = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_economy)):
+			if topic_economy_fortitle[i] in topic_economy[k] and topic_economy_fortitle[j] in topic_economy[k]:
+				economy_co_matrix_fortitle[i][j] = economy_co_matrix_fortitle[i][j] + 1
+
+economy_co_matrix_fortitle_output = open('economy_co_matrix_fortitle_output.txt','w')
+economy_co_matrix_fortitle_output.write(str(economy_co_matrix_fortitle))
+economy_co_matrix_fortitle_output.close()
+
+#topic_microsoft_fortitle
+
+topic_microsoft_fortitle = title_pertopic.filter(lambda x:x[1] == 'microsoft').map(lambda x:x[0]).take(100)
+topic_microsoft = title_forall.filter(lambda x:x[1] == 'microsoft').map(lambda x:x[0]).collect()
+microsoft_co_matrix_fortitle = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_microsoft)):
+			if topic_microsoft_fortitle[i] in topic_microsoft[k] and topic_microsoft_fortitle[j] in topic_microsoft[k]:
+				microsoft_co_matrix_fortitle[i][j] = microsoft_co_matrix_fortitle[i][j] + 1
+
+microsoft_co_matrix_fortitle_output = open('microsoft_co_matrix_fortitle_output.txt','w')
+microsoft_co_matrix_fortitle_output.write(str(microsoft_co_matrix_fortitle))
+microsoft_co_matrix_fortitle_output.close()
+
+#topic_palestine_fortitle
+
+topic_palestine_fortitle = title_pertopic.filter(lambda x:x[1] == 'palestine').map(lambda x:x[0]).take(100)
+topic_palestine = title_forall.filter(lambda x:x[1] == 'palestine').map(lambda x:x[0]).collect()
+palestine_co_matrix_fortitle = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_palestine)):
+			if topic_palestine_fortitle[i] in topic_palestine[k] and topic_palestine_fortitle[j] in topic_palestine[k]:
+				palestine_co_matrix_fortitle[i][j] = palestine_co_matrix_fortitle[i][j] + 1
+
+palestine_co_matrix_fortitle_output = open('palestine_co_matrix_fortitle_output.txt','w')
+palestine_co_matrix_fortitle_output.write(str(palestine_co_matrix_fortitle))
+palestine_co_matrix_fortitle_output.close()
+
+#topic_obama_forheadline
+
+topic_obama_forheadline = headline_pertopic.filter(lambda x:x[1] == 'obama').map(lambda x:x[0]).take(100)
+topic_obama = headline_forall.filter(lambda x:type(x[0]) == str).filter(lambda x:x[1] == 'obama').map(lambda x:x[0]).collect()
+obama_co_matrix_forheadline = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_obama)):
+			if topic_obama_forheadline[i] in topic_obama[k] and topic_obama_forheadline[j] in topic_obama[k]:
+				obama_co_matrix_forheadline[i][j] = obama_co_matrix_forheadline[i][j] + 1
+
+obama_co_matrix_forheadline_output = open('obama_co_matrix_forheadline_output.txt','w')
+obama_co_matrix_forheadline_output.write(str(obama_co_matrix_forheadline))
+obama_co_matrix_forheadline_output.close()
+
+#topic_economy_forheadline
+
+topic_economy_forheadline = headline_pertopic.filter(lambda x:x[1] == 'economy').map(lambda x:x[0]).take(100)
+topic_economy = headline_forall.filter(lambda x:type(x[0]) == str).filter(lambda x:x[1] == 'economy').map(lambda x:x[0]).collect()
+economy_co_matrix_forheadline = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_economy)):
+			if topic_economy_forheadline[i] in topic_economy[k] and topic_economy_forheadline[j] in topic_economy[k]:
+				economy_co_matrix_forheadline[i][j] = economy_co_matrix_forheadline[i][j] + 1
+
+economy_co_matrix_forheadline_output = open('economy_co_matrix_forheadline_output.txt','w')
+economy_co_matrix_forheadline_output.write(str(economy_co_matrix_forheadline))
+economy_co_matrix_forheadline_output.close()
+
+#topic_microsoft_forheadline
+
+topic_microsoft_forheadline = headline_pertopic.filter(lambda x:x[1] == 'microsoft').map(lambda x:x[0]).take(100)
+topic_microsoft = headline_forall.filter(lambda x:type(x[0]) == str).filter(lambda x:x[1] == 'microsoft').map(lambda x:x[0]).collect()
+microsoft_co_matrix_forheadline = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_microsoft)):
+			if topic_microsoft_forheadline[i] in topic_microsoft[k] and topic_microsoft_forheadline[j] in topic_microsoft[k]:
+				microsoft_co_matrix_forheadline[i][j] = microsoft_co_matrix_forheadline[i][j] + 1
+
+microsoft_co_matrix_forheadline_output = open('microsoft_co_matrix_forheadline_output.txt','w')
+microsoft_co_matrix_forheadline_output.write(str(microsoft_co_matrix_forheadline))
+microsoft_co_matrix_forheadline_output.close()
+
+#topic_palestine_forheadline
+
+topic_palestine_forheadline = headline_pertopic.filter(lambda x:x[1] == 'palestine').map(lambda x:x[0]).take(100)
+topic_palestine = headline_forall.filter(lambda x:type(x[0]) == str).filter(lambda x:x[1] == 'palestine').map(lambda x:x[0]).collect()
+palestine_co_matrix_forheadline = np.zeros([100,100])
+
+for i in range(100):
+	for j in range(100):
+		for k in range (len(topic_palestine)):
+			if topic_palestine_forheadline[i] in topic_palestine[k] and topic_palestine_forheadline[j] in topic_palestine[k]:
+				palestine_co_matrix_forheadline[i][j] = palestine_co_matrix_forheadline[i][j] + 1
+
+palestine_co_matrix_forheadline_output = open('palestine_co_matrix_forheadline_output.txt','w')
+palestine_co_matrix_forheadline_output.write(str(palestine_co_matrix_forheadline))
+palestine_co_matrix_forheadline_output.close()
+
